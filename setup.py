0205247b03d7bbeaf5023dbef789672ce606abfc
philgras <thebigone@grassal-mueller.de>
2022-04-25 16:26:32 +0200

    fixed comma error

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     python_requires=">=3.9",
     install_requires=[
         "ConfigArgParse",
-        "numpy==1.21"
+        "numpy==1.21",
         "torch@https://download.pytorch.org/whl/cu113/torch-1.10.2%2Bcu113-cp39-cp39-linux_x86_64.whl",
         "torchvision@https://download.pytorch.org/whl/cu113/torchvision-0.11.3%2Bcu113-cp39-cp39-linux_x86_64.whl",
         "pytorch3d@https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py39_cu113_pyt1100/pytorch3d-0.6.1-cp39-cp39-linux_x86_64.whl",
